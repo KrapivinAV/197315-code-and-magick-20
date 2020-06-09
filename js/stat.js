d27c06f13ad701578b591c858a5a004e98d8eaa8
Крапивин Александр Владимирович <@COMPLEX.LOCAL>
2020-06-09 10:29:55 +0700

    Устраняет замечания Наставника к заданию по главе 2

--- js/stat.js
+++ js/stat.js
@@ -4,12 +4,15 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var CLOUD_COLOR = '#fff';
+var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var GAP = 5;
 var FONT_GAP = 20;
+var FONT_COLOR = '#000';
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var BAR_GAP = 50;
-var PRIMARY_BAR_COLOR = 'rgba(255, 0, 0, 1)';
+var BAR_COLOR_PRIMARY = 'rgba(255, 0, 0, 1)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -28,15 +31,15 @@ var getMaxElement = function (arr) {
 };
 
 var getColor = function (player) {
-  var secondaryBarColor = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
-  return player === 'Вы' ? PRIMARY_BAR_COLOR : secondaryBarColor;
+  var barColorSecondary = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
+  return player === 'Вы' ? BAR_COLOR_PRIMARY : barColorSecondary;
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + 2 * GAP, CLOUD_Y + 2 * GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_X + 2 * GAP, CLOUD_Y + 2 * GAP, CLOUD_SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = FONT_COLOR;
 
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
@@ -46,7 +49,7 @@ window.renderStatistics = function (ctx, players, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = FONT_COLOR;
     ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP + BAR_GAP * (i + 1) + BAR_WIDTH * i, CLOUD_Y + 2 * GAP + 3 * FONT_GAP + (BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime));
     ctx.fillText(players[i], CLOUD_X + GAP + BAR_GAP * (i + 1) + BAR_WIDTH * i, CLOUD_Y + 5 * FONT_GAP + BAR_HEIGHT);
     ctx.fillStyle = getColor(players[i]);
